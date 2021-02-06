877a5550a39608292069313a2435c5e6a9400649
Wadie Habib <77555267+mans0amador@users.noreply.github.com>
2021-02-07 00:07:19 +0200

    raphael: dt2w package

--- Android.mk
+++ Android.mk
@@ -6,6 +6,14 @@
 
 LOCAL_PATH := $(call my-dir)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE       := dt2w.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := rootdir/ etc/dt2w.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/init
+include $(BUILD_PREBUILT)
+
 ifneq ($(filter raphael,$(TARGET_DEVICE)),)
   subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH))
   $(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
---
+++ rootdir/bin/init.dt2w.sh
@@ -0,0 +1,2 @@
+#! /system/bin/sh
+sendevent /dev/input/event4 0 1 5
