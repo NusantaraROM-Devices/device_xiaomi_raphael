4e8176e21ee5ff900bd28352a8913ae8ea940e8b
Wadie Habib <77555267+mans0amador@users.noreply.github.com>
2021-02-13 14:50:08 +0200

    raphael: overlay: update Nusantara overlay+

--- Android.mk
+++ Android.mk
@@ -10,7 +10,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE       := dt2w.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := rootdir/ etc/dt2w.rc
+LOCAL_SRC_FILES    := rootdir/etc/dt2w.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/init
 include $(BUILD_PREBUILT)
 
