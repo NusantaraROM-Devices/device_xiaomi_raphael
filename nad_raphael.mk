9d4a1f782f9ca96979d0fdfa99d1c40811573c0f
mans0amador <stardroid.sy@gmail.com>
2021-02-25 23:34:13 +0000

    raphael: Add NusantaraParts

--- nad_raphael.mk
+++ nad_raphael.mk
@@ -8,10 +8,12 @@ $(call inherit-product, device/xiaomi/raphael/device.mk)
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/nusantara/config/common_full_phone.mk)
+$(call inherit-product, packages/apps/NusantaraParts/nadproject.mk)
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_BRAND := Xiaomi
@@ -19,11 +21,12 @@ PRODUCT_DEVICE := raphael
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_NAME := nad_raphael
 
-BUILD_FINGERPRINT := "Xiaomi/raphaelin/raphaelin:9/PKQ1.181121.001/V10.3.3.0.PFKINXM:user/release-keys"
+BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ1A.210205.004/7038034:user/release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 NAD_BUILD_TYPE := OFFICIAL
+#USE_GAPPS ?= true
 
 TARGET_BOOT_ANIMATION_RES := 1080
 
